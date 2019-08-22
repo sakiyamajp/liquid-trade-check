aa963de5cf47a5114b7e400c797d108b196d9cf0
akiyama <akiyama@sssagi.net>
2019-08-22 20:17:56 +0900

    change port

--- dist/server.js
+++ dist/server.js
@@ -1,7 +1,7 @@
 var StaticServer = require('static-server');
 var server = new StaticServer({
   rootPath: './dist',            // required, the root of the server file tree
-  port: 3000,               // required, the port to listen
+  port: process.env.PORT || 3000,               // required, the port to listen
   name: 'sakiyama',   // optional, will set "X-Powered-by" HTTP header
   // host: '10.0.0.100',       // optional, defaults to any interface
   // cors: '*',                // optional, defaults to undefined
