1ae02b91a2f30a6c11b7d06bccf46e30cf7de29e
sakiyamajp <s@sssagi.net>
2019-09-20 06:04:40 +0900

    d

--- dist/js/report.js
+++ dist/js/report.js
@@ -207,7 +207,7 @@ async function getFromLiquid(id, token) {
   const liquid = new ccxt.liquid({
     apiKey: id,
     secret: token,
-    proxy: 'http://crosproxy.saky.work/'
+    // proxy: 'http://crosproxy.saky.work/'
   });
   let table = {};
   let max;
